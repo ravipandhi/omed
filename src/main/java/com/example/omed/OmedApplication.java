21899c7e480eb94b854792ffc90fe62a04a4939f
ravipandhi <rpandhi2018@my.fit.edu>
2021-08-09 20:56:09 -0400

    init

---
+++ src/main/java/com/example/omed/OmedApplication.java
@@ -0,0 +1,13 @@
+package com.example.omed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omed/OmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
